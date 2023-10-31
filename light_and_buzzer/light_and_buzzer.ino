2c47c636d370969b8e572fad8f505460a3a01a44
DoubleU-ANG <100694831+DoubleU-ANG@users.noreply.github.com>
2023-10-31 22:16:32 +0000

    adding comments

--- light_and_buzzer/light_and_buzzer.ino
+++ light_and_buzzer/light_and_buzzer.ino
@@ -5,12 +5,13 @@ const int trigPin = 9;        //assign pin 9 to trigger
 const int echoPin = 10;       //assign pin 10 to echo
 const int LED = 13;           //keep the LED on pin 13
 const int BUZZER = 8;         //keep the buzzer on pin 8
-const boolean debug = false;  //variable to show hide debug info
+const boolean debug = true;  //variable to show hide debug info
 
 // establish variables for duration of the ping, and the distance result in centimeters and one for LED blink rate:
 long duration, cm;
 int blinkrate = 0;
 
+// calculate the distance
 long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
 }
@@ -29,13 +30,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //make the ultrasonic work
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
 digitalWrite(trigPin, HIGH);
 delayMicroseconds(10);
 digitalWrite(trigPin, LOW);
+//getting the time by timing the high volts
 duration = pulseIn(echoPin, HIGH);
 cm = microsecondsToCentimeters(duration);
+//changing blinking rate based on distance
 if(cm < 100){
 
   if(cm < 30){
@@ -49,12 +53,13 @@ if(cm < 100){
   }
 
   if(debug){
+    //print the distance on computer by serial communication
     Serial.print(" cm: ");
     Serial.print(cm);
   }
   digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(BUZZER, HIGH);// turn the BUZZER on
-  delay(blinkrate);                       // wait for a second
+  delay(blinkrate);                       // delay based on the distance
   digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
   digitalWrite(BUZZER, LOW);// turn the BUZZER off
   delay(blinkrate);
