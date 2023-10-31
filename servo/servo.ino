718e0b6656e48d25ae87475319e4ebc6ecabbee8
DoubleU-ANG <100694831+DoubleU-ANG@users.noreply.github.com>
2023-10-31 22:27:24 +0000

    adding comments

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,21 +1,21 @@
-#include <Servo.h>    // 声明调用Servo.h库
-Servo myservo;        // 创建一个舵机对象
-int pos = 0;          // 变量pos用来存储舵机位置
+#include <Servo.h>  
+Servo myservo;        // create a servo object 
+int pos = 0;          // define pos for storing the degree that servo rotates
 
 void servo_move()
 {
-     for(pos = 0; pos < 180; pos += 1){    // 舵机从0°转到180°，每次增加1°          
-      myservo.write(pos);           // 给舵机写入角度   
-      delay(1);                    // 延时15ms让舵机转到指定位置
+     for(pos = 0; pos < 180; pos += 1){         
+      myservo.write(pos);           // make servo rotate from 0 degree to 180 degree  
+      delay(1);                    
    }
-    for(pos = 180; pos>=1; pos-=1) {    // 舵机从180°转回到0°，每次减小1°                               
-       myservo.write(pos);        // 写角度到舵机     
-       delay(11);                 // 延时15ms让舵机转到指定位置
+    for(pos = 180; pos>=1; pos-=1) {                                  
+       myservo.write(pos);        // make servo rotate from 180 degree to 0 degree    
+       delay(11);                 
     } 
 }
 
 void setup() { 
-    myservo.attach(9);  // 将引脚9上的舵机与声明的舵机对象连接起来
+    myservo.attach(9);  // connect the servo with pin 9
 } 
 
 void loop() { 
